db7d7c16348802c34556bcdad10936e3e81150d2
wangchunbo <76217148@qq.com>
2019-08-29 16:37:25 +0800

    调整

--- itcast-python/cn/runoob/python3/SMTP发送邮件.py
+++ itcast-python/cn/runoob/python3/SMTP发送邮件.py
@@ -2,8 +2,8 @@ import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
 
-sender = 'wsxcd@wsecar.com'
-receivers = ['wangchunbo@wsecar.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
+sender = '****@wsecar.com'
+receivers = ['***@wsecar.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
 
 # 三个参数：第一个为文本内容，第二个 plain 设置文本格式，第三个 utf-8 设置编码
 message = MIMEText('Python 邮件发送测试...', 'plain', 'utf-8')
@@ -16,9 +16,9 @@ try:
     # 创建对象
     smtpObj = smtplib.SMTP()
     # 连接邮件服务器
-    smtpObj.connect("smtp.wsecar.com", 25)
+    smtpObj.connect("smtp.***.com", 25)
     # 登录，用户名，密码
-    smtpObj.login("wsxcd@wsecar.com", "Wsjc201609")
+    smtpObj.login("***@***.com", "***")
     # 发送邮件
     smtpObj.sendmail(sender, receivers, message.as_string())
     print("邮件发送成功")
